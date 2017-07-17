f10bbddc90a849080b671e63cd78a840acd44797
Artyom Svetashov <arsvetashov@gmail.com>
2017-07-17 20:53:44 +0300

    Second Commit

--- core/src/com/svetashov/dotbydot/model/GameWorld.java
+++ core/src/com/svetashov/dotbydot/model/GameWorld.java
@@ -7,15 +7,18 @@ import java.util.ArrayList;
 public class GameWorld {
 
     private ArrayList<SimpleDot> arrayOfDots;
+    private SimpleDot startDot, endDot;
     private Vector2 sizeOfMap;
     public PlayerDot playerDot;
 
-    public GameWorld(Vector2 sizeOfMap, ArrayList<SimpleDot> dots){
+    public GameWorld(Vector2 sizeOfMap, ArrayList<SimpleDot> dots, SimpleDot startDot, SimpleDot endDot){
         this.arrayOfDots = dots;
         this.sizeOfMap = sizeOfMap;
+        this.startDot = startDot;
+        this.endDot = endDot;
 
         // player is placing on the start dot
-        playerDot.setPosition(arrayOfDots.get(0).getPosition());
+        playerDot.setPosition(this.startDot.getPosition());
     }
 
     public ArrayList<SimpleDot> getArrayOfDots() {
